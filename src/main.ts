bc06b664b565e2994e0307ffce20afc50776987d
antonpolieshchuk <anton2013ua@gmail.com>
2023-02-02 14:12:40 +0200

    Revert "add fake ssl"
    This reverts commit c9d4265065b40f5f041911aecb6e5637f3b94ddc.

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { Environment } from './environment';
 import { readFileSync } from 'fs';
-import * as https from 'https';
-import * as http from 'http';
-import * as express from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const key = readFileSync(
-    __dirname + '/../ssl_fake/84-246.85.252_3000-privateKey.key',
-  );
+  const key = readFileSync(__dirname + '/../ssl_fake/84-246.85.252_3000-privateKey.key');
   const cert = readFileSync(__dirname + '/../ssl_fake/84-246.85.252_3000.crt');
-  const httpsOptions = { key, cert };
 
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key,
+      cert,
+    },
+  });
   app.enableCors();
   const configService: ConfigService<Environment> = app.get(ConfigService);
   const config = new DocumentBuilder()
@@ -27,12 +24,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.init();
-
-  const httpsServer = https.createServer(httpsOptions, server);
-  httpsServer.listen(configService.get<number>('PORT'));
-
-  const httpServer = http.createServer(server);
-  httpServer.listen(3001);
+  await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
